a3b168e27abbfe7e6f13ee23d1894125f25ad1b1
tessied <tessied@uchicago.edu>
2021-08-28 15:11:02 -0400

    create initial snake body

--- main.py
+++ main.py
@@ -5,4 +5,10 @@ screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("Snake Game")
 
+for index in range(3):
+    new_turtle = Turtle(shape="square")
+    new_turtle.color("white")
+    new_turtle.goto(0 - index * 20, 0)
+
+
 screen.exitonclick()
